68ddcfd742ce488ba43533595953508afba238b3
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2020-04-15 17:52:21 +0300

    remake)

--- population.sql
+++ population.sql
@@ -1,29 +1,18 @@
---years_stat population
-INSERT INTO  YEARS_STAT(stat_year) VALUES (2015);
-INSERT INTO  YEARS_STAT(stat_year) VALUES (2016);
-INSERT INTO  YEARS_STAT(stat_year) VALUES (2017);
-INSERT INTO  YEARS_STAT(stat_year) VALUES (2018);
+-- States population 
+INSERT INTO States(city_state) VALUES ('Alabama');
+INSERT INTO States(city_state) VALUES ('Alaska');
+INSERT INTO States(city_state) VALUES ('Arkansas');
+INSERT INTO States(city_state) VALUES ('California');
+
 
 --full_city_address population
 INSERT INTO full_city_address(city_name, city_state) VALUES ('Autauga','Alabama');
 INSERT INTO full_city_address(city_name, city_state) VALUES ('Baldwin','Alabama');
-INSERT INTO full_city_address(city_name, city_state) VALUES ('Bibb','Alabama');
-INSERT INTO full_city_address(city_name, city_state) VALUES ('Bristol Bay Borough','Alaska');
+INSERT INTO full_city_address(city_name, city_state) VALUES ('Bethel Census Area','Alaska');
+INSERT INTO full_city_address(city_name, city_state) VALUES ('Los Angeles','California');
 
---percent_years population
-INSERT INTO percent_years(city_name,city_state,stat_year,city_percent) VALUES('Autauga','Alabama',2015,0.5);
-INSERT INTO percent_years(city_name,city_state,stat_year,city_percent) VALUES('Baldwin','Alabama',2016,-3.2);
-INSERT INTO percent_years(city_name,city_state,stat_year,city_percent) VALUES('Bibb','Alabama',2015,1.5);
-INSERT INTO percent_years(city_name,city_state,stat_year,city_percent) VALUES('Bristol Bay Borough','Alaska',2015,0.1);
-
---gdp_years population
-INSERT INTO gdp_years(city_name,city_state,stat_year,gdp) VALUES('Autauga','Alabama',2015,1582473);
-INSERT INTO gdp_years(city_name,city_state,stat_year,gdp) VALUES('Baldwin','Alabama',2016,302032);
-INSERT INTO gdp_years(city_name,city_state,stat_year,gdp) VALUES('Bibb','Alabama',2015,303);
-INSERT INTO gdp_years(city_name,city_state,stat_year,gdp) VALUES('Bristol Bay Borough','Alaska',2015,321200);
-
---rating_years population
-INSERT INTO rating_years(city_name,city_state,stat_year,rating) VALUES('Autauga','Alabama',2015,70);
-INSERT INTO rating_years(city_name,city_state,stat_year,rating) VALUES('Baldwin','Alabama',2016,2);
-INSERT INTO rating_years(city_name,city_state,stat_year,rating) VALUES('Bibb','Alabama',2015,5);
-INSERT INTO rating_years(city_name,city_state,stat_year,rating) VALUES('Bristol Bay Borough','Alaska',2015,10);
+--city_info population
+INSERT INTO city_info(city_name,city_state,stat_year,city_gdp,city_rating) VALUES ('Autauga','Alabama',2015,102031,40);
+INSERT INTO city_info(city_name,city_state,stat_year,city_gdp,city_rating) VALUES ('Baldwin','Alabama',2015,10232134,50);
+INSERT INTO city_info(city_name,city_state,stat_year,city_gdp,city_rating) VALUES ('Bethel Census Area','Alaska',2016,1021,3);
+INSERT INTO city_info(city_name,city_state,stat_year,city_gdp,city_rating) VALUES ('Los Angeles', 'California',2015,23041,42);
--- workshop-2.sql
+++ workshop-2.sql
@@ -1,40 +1,26 @@
-CREATE TABLE years_stat(
-stat_year NUMBER(4, 0) NOT NULL PRIMARY KEY CHECK (stat_year > 0)
+CREATE TABLE States(
+city_state VARCHAR(20) NOT NULL PRIMARY KEY
 );
-
 CREATE TABLE Full_city_address(
 city_name VARCHAR(20) NOT NULL,
-city_state VARCHAR(20) NOT NULL,
+city_state VARCHAR(20) NOT NULL REFERENCES States(city_state),
 CONSTRAINT PK_Full_city_address PRIMARY KEY (city_name, city_state)
 );
 
-CREATE TABLE percent_years(
+CREATE TABLE city_info(
 city_name VARCHAR(20) NOT NULL,
 city_state VARCHAR(20) NOT NULL,
-stat_year NUMBER(4, 0) NOT NULL REFERENCES YEARS_STAT(stat_year),
-city_percent NUMBER(3,2) NOT NULL CHECK (city_percent < 100.00),
+stat_year NUMBER(4, 0) NOT NULL CHECK (stat_year > 0),
+city_gdp NUMBER(10,0) NOT NULL,
+city_rating NUMBER(4,0) NOT NULL,
 CONSTRAINT PK_percent_years PRIMARY KEY (city_name, city_state, stat_year),
 CONSTRAINT FK_percent_years FOREIGN KEY (city_name, city_state) REFERENCES Full_city_address(city_name,city_state)
 );
 
 
-CREATE TABLE gdp_years(
-city_name VARCHAR(20) NOT NULL,
-city_state VARCHAR(20) NOT NULL,
-stat_year NUMBER(4, 0) NOT NULL REFERENCES years_stat(stat_year) CHECK (stat_year > 0),
-gdp NUMBER(20,0) NOT NULL CHECK (gdp >= 0),
-CONSTRAINT PK_gdp_years PRIMARY KEY (city_name, city_state, stat_year),
-CONSTRAINT FK_gdp_years FOREIGN KEY (city_name, city_state) REFERENCES Full_city_address(city_name,city_state)
-);
 
-CREATE TABLE rating_years(
-city_name VARCHAR(20) NOT NULL,
-city_state VARCHAR(20) NOT NULL,
-stat_year NUMBER(4, 0) NOT NULL REFERENCES years_stat(stat_year) CHECK (stat_year > 0),
-rating NUMBER(4) NOT NULL CHECK (rating > 0),
-CONSTRAINT PK_rating_years PRIMARY KEY (city_name, city_state, stat_year),
-CONSTRAINT FK_rating_years FOREIGN KEY (city_name, city_state) REFERENCES Full_city_address(city_name,city_state)
-);
+
+
 
 
 
