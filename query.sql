9be06f67eb97acd385dc293f2cc7e9605eff0e6b
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2020-04-16 11:23:28 +0300

    next step

--- query.sql
+++ query.sql
@@ -1,24 +1,28 @@
-/*Запит 1 - Вивести GDP за 2015 рік. Місто - число
-Запит 2 -  Вивести GDP міст за весь час. Місто - число
-Запит 3 - Вивести графік зміни рейтингу міста за кожен рік. Рік - рейтинг*/
+/*пїЅпїЅпїЅпїЅпїЅ 1 - пїЅпїЅпїЅпїЅпїЅпїЅпїЅ GDP пїЅпїЅ 2015 пїЅпїЅ. МіпїЅпїЅпїЅ - пїЅпїЅпїЅпїЅпїЅ
+пїЅпїЅпїЅпїЅпїЅ 2 -  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ GDP пїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ. МіпїЅпїЅпїЅ - пїЅпїЅпїЅпїЅпїЅ
+пїЅпїЅпїЅпїЅпїЅ 3 - пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ. РіпїЅ - пїЅпїЅпїЅпїЅпїЅпїЅпїЅ*/
 
 --query 1
-SELECT c1.city_name || ', ' || c1.city_state AS city_name
-,city_gdp
+SELECT
+    c1.city_name || ', ' || c1.city_state AS city_name
+    ,city_gdp
 FROM city_info c1 
-join full_city_address c2 
+JOIN full_city_address c2
 ON c1.city_name = c2.city_name
 WHERE c1.stat_year = 2015;
 
 --query 2
-SELECT c1.city_name || ', ' || c1.city_state AS city_name
-, SUM(c1.city_gdp) AS sum_gdp
+SELECT
+    c1.city_name || ', ' || c1.city_state AS city_name
+    , SUM(c1.city_gdp) AS sum_gdp
 FROM city_info c1
 GROUP BY c1.city_name, c1.city_state;
 
 --query 3
 
-SELECT c1.city_name || ', ' || c1.city_state AS city_name, c1.stat_year, c1.city_rating
+SELECT
+    c1.city_name || ', ' || c1.city_state AS city_name, c1.stat_year
+    , c1.city_rating
 FROM city_info c1
 WHERE c1.city_name = 'Autauga' AND c1.city_state = 'Alabama'
 ORDER BY c1.city_name;
